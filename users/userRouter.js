791962e0037bbccedbd797135908f10928d62fba
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-30 16:12:53 -0400

    adds get user by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -25,7 +25,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+    const id = Number(req.params.id)
+    id ?
+    userDb.getById(id)
+    .then(user => 
+        user ? res.status(200).json(user) : res.status(404).json({ error: 'That user does not exist'})
+        )
+    .catch(err => res.status(500).json({ error: 'There was an error fetching the user'}))
+    :
+    res.status(400).json({ error: 'Please include a valid id'})
 });
 
 router.get('/:id/posts', (req, res) => {
