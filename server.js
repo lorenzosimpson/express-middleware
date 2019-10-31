3edd9fc87549e8eb111eac6cb2cc179fe2d704e0
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-31 15:24:18 -0400

    modifies welcome message at /api

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use('/api/users', userRouter)
 server.use('/api/posts', postRouter)
 
 server.get('/api', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Welcome to API! Navigate to /users to see users</h2>`)
 });
 
 
