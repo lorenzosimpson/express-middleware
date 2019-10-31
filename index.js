ccc985b3c5cc86a3149a687f70af8ecaa03dd3e0
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-31 15:19:10 -0400

    adds .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const server = require('./server');
 
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== Server running on port ${port} ===\n`))
